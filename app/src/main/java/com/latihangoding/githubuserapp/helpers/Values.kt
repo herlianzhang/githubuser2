aa525c0ce73b5f1beb2db3cf02d4614ed09958f1
herlianzhang <herlianzhang370@gmail.com>
2020-07-30 16:59:29 +0700

    mini update

--- app/src/main/java/com/latihangoding/githubuserapp/helpers/Values.kt
+++ app/src/main/java/com/latihangoding/githubuserapp/helpers/Values.kt
@@ -2,5 +2,5 @@ package com.latihangoding.githubuserapp.helpers
 
 object Values {
     const val MAIN_URL = "https://api.github.com/"
-    const val TOKEN = "token d4d8a4628a141c41f3bbd262fb05f4231fd61a04"
+    const val TOKEN = "TOKEN (USER YOU TOKEN)"
 }
