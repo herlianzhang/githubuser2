fcf3c1446f9507ba23e2fc1e12bbbb55217e6bc6
herlianzhang <herlianzhang370@gmail.com>
2020-07-31 13:01:58 +0700

    github2 submission revision

--- app/src/main/java/com/latihangoding/githubuserapp/helpers/Values.kt
+++ app/src/main/java/com/latihangoding/githubuserapp/helpers/Values.kt
@@ -2,5 +2,5 @@ package com.latihangoding.githubuserapp.helpers
 
 object Values {
     const val MAIN_URL = "https://api.github.com/"
-    const val TOKEN = "TOKEN (USER YOU TOKEN)"
+    const val TOKEN = "TOKEN 374232b0fef049dd54ac47fb7eb38582b7544210"
 }
